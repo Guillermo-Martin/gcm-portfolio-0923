eb2ecbdd4282229acf336cf4cdd74df0ca17a2d3
Guillermo Martin <gscalica@gmail.com>
2023-09-29 14:28:20 -0700

    Create toggle mobile nav menu functionality

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,24 +1,29 @@
 let body = document.querySelector("body");
+let mobileNavMenu = document.getElementById("mobile-nav-menu");
 
+console.log(mobileNavMenu)
 
 // hamburger menu functionality
 // 1. target the hamburger icon
 let hamburgerIcon = document.getElementById("hamburger-icon");
+let mobileNavClose = document.createElement("p");
 
 // 2. add a click listener
 hamburgerIcon.addEventListener("click", () => {
   // on click, create a div
-  let mobileNavMenu = document.createElement("div");
+  // let mobileNavMenu = document.createElement("div");
+  mobileNavMenu.classList.toggle("show");
 
-  // add a class for styling
-  mobileNavMenu.classList.add("mobile-nav-menu");
-
-  // add it to the body
-  body.appendChild(mobileNavMenu);
-
-  // give it a z-index greater than the body
+  // // add a class for styling
+  // mobileNavMenu.classList.add("mobile-nav-menu");
 
   
+  // mobileNavClose.classList.add("mobile-nav-close-button");
+  // mobileNavClose.textContent = "X";
+  // mobileNavMenu.appendChild(mobileNavClose);
+
+  // // add it to the body
+  // body.appendChild(mobileNavMenu);
 });
 
 
