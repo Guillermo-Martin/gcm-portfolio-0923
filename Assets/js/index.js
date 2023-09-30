1edc2b33685ffc66c74699a5ad70e940562beaa8
Guillermo Martin <gscalica@gmail.com>
2023-09-29 20:48:31 -0700

    Add show/hide mobile nav menu functionality

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,29 +1,33 @@
 let body = document.querySelector("body");
 let mobileNavMenu = document.getElementById("mobile-nav-menu");
 
-console.log(mobileNavMenu)
 
 // hamburger menu functionality
 // 1. target the hamburger icon
 let hamburgerIcon = document.getElementById("hamburger-icon");
+let closeIcon = document.getElementById("close-icon");
 let mobileNavClose = document.createElement("p");
 
 // 2. add a click listener
 hamburgerIcon.addEventListener("click", () => {
-  // on click, create a div
-  // let mobileNavMenu = document.createElement("div");
-  mobileNavMenu.classList.toggle("show");
+  // show menu
+  mobileNavMenu.classList.add("show");
 
-  // // add a class for styling
-  // mobileNavMenu.classList.add("mobile-nav-menu");
+  // hide hamburger icon
+  hamburgerIcon.classList.add("hide");
 
-  
-  // mobileNavClose.classList.add("mobile-nav-close-button");
-  // mobileNavClose.textContent = "X";
-  // mobileNavMenu.appendChild(mobileNavClose);
-
-  // // add it to the body
-  // body.appendChild(mobileNavMenu);
+  // show close icon
+  closeIcon.classList.add("show");
 });
 
+closeIcon.addEventListener("click", () => {
+  // hide menu
+  mobileNavMenu.classList.remove("show");
+
+  // hide the close icon
+  closeIcon.classList.remove("show");
+
+  // show the hamburger icon
+  hamburgerIcon.classList.remove("hide");
+});
 
