0739c2cb7a417e98b7539d9859887c11de5458a8
Guillermo Martin <gscalica@gmail.com>
2023-11-08 15:51:25 -0800

    Add table of contents button arrow functionality

--- Assets/js/case-study.js
+++ Assets/js/case-study.js
@@ -1,6 +1,8 @@
 // targeted elements
 let tableOfContentsBtn = document.getElementById("table-of-contents-btn");
 let tableOfContents = document.getElementById("mobile-table-of-contents");
+let downArrow = document.getElementById("down-arrow");
+let upArrow = document.getElementById("up-arrow");
 
 // ---------- Table of contents mobile functionality ----------
 tableOfContentsBtn.addEventListener("click", () => {
@@ -8,5 +10,9 @@ tableOfContentsBtn.addEventListener("click", () => {
 
   // on click, show the menu
   tableOfContents.classList.toggle("show");
+
+  // hide the down arrow, show the up arrow
+  downArrow.classList.toggle("hide");
+  upArrow.classList.toggle("show");
 
 });
