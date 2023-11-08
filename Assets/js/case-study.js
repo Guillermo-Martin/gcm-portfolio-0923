e2b2eca42510df001aa2212d770ea67615f08286
Guillermo Martin <gscalica@gmail.com>
2023-11-07 21:45:59 -0800

    Add show table of contents functionality to table of contents button

--- Assets/js/case-study.js
+++ Assets/js/case-study.js
@@ -1,7 +1,12 @@
 // targeted elements
-let mobileTableOfContents = document.getElementById("table-of-contents");
+let tableOfContentsBtn = document.getElementById("table-of-contents");
+let tableOfContents = document.getElementById("mobile-table-of-contents");
 
 // ---------- Table of contents mobile functionality ----------
-mobileTableOfContents.addEventListener("click", () => {
-  alert("you clicked on the button!");
+tableOfContentsBtn.addEventListener("click", () => {
+  // alert("you clicked on the button!");
+
+  // on click, show the menu
+  tableOfContents.classList.add("show");
+
 });
