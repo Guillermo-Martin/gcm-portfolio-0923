b8631ae847fb1fe3c95ac7ff619c9df4ecbf6434
Guillermo Martin <gscalica@gmail.com>
2023-11-07 21:33:45 -0800

    Create Javascript file for case studies, add click listener to table of contents button

---
+++ Assets/js/case-study.js
@@ -0,0 +1,7 @@
+// targeted elements
+let mobileTableOfContents = document.getElementById("table-of-contents");
+
+// ---------- Table of contents mobile functionality ----------
+mobileTableOfContents.addEventListener("click", () => {
+  alert("you clicked on the button!");
+});
