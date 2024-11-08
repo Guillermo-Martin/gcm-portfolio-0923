c5678711f0e78fed11b711356f9a2af326a6dc89
Guillermo Martin <gscalica@gmail.com>
2024-11-07 18:27:18 -0800

    Refactor scrollTrigger table of contents link animation

--- assets/js/about.js
+++ assets/js/about.js
@@ -3,32 +3,28 @@ console.log("connected!!!");
 // register scrollToPlugin
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
-// gsap.to(window, { duration: 2, scrollTo: 400 });
-
 // get all of the table of contents link
 const tocLinkArr = document.querySelectorAll(".table-of-contents-link");
 
 // loop through the table of contents links and add an eventlistener
 for(let i = 0; i < tocLinkArr.length; i++) {
+  // get the section id to scroll to and define the scrollTrigger target
+  let sectionId = tocLinkArr[i].dataset.section;
+  let scrollTriggerTarget = `.table-of-contents-link[data-section='${sectionId}']`;
+
   // add event listener
   tocLinkArr[i].addEventListener("click", () => {
-    // get the section id to scroll to
-    let sectionId = tocLinkArr[i].dataset.section;
-
     // when user clicks on the link, scroll to that section
     gsap.to(window, { duration: 1, scrollTo: `#${sectionId}` });
   });
 
-  // add the scrollTrigger animation
+  // add the scrollTrigger "active" link animation
+  gsap.to(scrollTriggerTarget, {
+    scrollTrigger: {
+      trigger: `#${sectionId}`,
+      start: "top 35%",
+      end: "bottom 50%",
+      toggleClass: {targets: scrollTriggerTarget, className: "active"}
+    }
+  });
 };
-
-// scrolltrigger animations
-gsap.to(".table-of-contents-link[data-section='experience']", {
-  scrollTrigger: {
-    trigger: "#experience",
-    start: "top 35%",
-    end: "bottom 50%",
-    markers: true,
-    toggleClass: {targets: ".table-of-contents-link[data-section='experience']", className: "active"}
-  }
-})
