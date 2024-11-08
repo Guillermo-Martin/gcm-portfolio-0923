53082e1bc0b43bf84085087ed19d80ec80081de5
Guillermo Martin <gscalica@gmail.com>
2024-11-07 16:10:03 -0800

    Add 'scrollTo' effect, change links in table of contents to p tags to get get 'scrollTo' effect to work

---
+++ assets/js/about.js
@@ -0,0 +1,21 @@
+console.log("connected!!!");
+
+// register scrollToPlugin
+gsap.registerPlugin(ScrollToPlugin);
+
+// gsap.to(window, { duration: 2, scrollTo: 400 });
+
+// get all of the table of contents link
+const tocLinkArr = document.querySelectorAll(".table-of-contents-link");
+
+// loop through the table of contents links and add an eventlistener
+for(let i = 0; i < tocLinkArr.length; i++) {
+  // add event listener
+  tocLinkArr[i].addEventListener("click", () => {
+    // get the section id to scroll to
+    let sectionId = tocLinkArr[i].dataset.section;
+
+    // when user clicks on the link, scroll to that section
+    gsap.to(window, { duration: 1, scrollTo: `#${sectionId}` });
+  });
+};
