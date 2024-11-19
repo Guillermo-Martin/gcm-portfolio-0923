880afb217b4367b0442a931559acb2151a5327a5
Guillermo Martin <gscalica@gmail.com>
2024-11-18 18:10:39 -0800

    Add 'IntersectionObserver' to detect when a section enters the viewport

--- assets/js/kumiko-music-redesign.js
+++ assets/js/kumiko-music-redesign.js
@@ -1,1 +1,23 @@
 console.log("connected to kumiko music redesign!");
+
+// get all sections
+const allSections = document.querySelectorAll(".main-content-section");
+
+// function to run when section is observed
+const observerFunction = (entries) => {
+  // for each section, check to see if it is intersecting the viewport
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      // Do something when the section enters the viewport
+      console.log('Section entered:', entry.target);
+    };
+  });
+};
+
+// create an intersection observer
+const observer = new IntersectionObserver(observerFunction);
+
+// for each section, observe when it enters the viewport
+allSections.forEach(section => {
+  observer.observe(section);
+});
