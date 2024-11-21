48fb1523f7303ef478bc0c178a32fcca89ce334e
Guillermo Martin <gscalica@gmail.com>
2024-11-21 09:49:38 -0800

    Revise page load animation to fix 'flash of unstyled content' with greensock

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,13 +1,20 @@
 console.log("connected to about!!!");
 
-window.onload = () => {
+// ---------- Page load animation function ----------
+const init = () => {
   gsap.timeline()
     .set("body", {overflow: "hidden"})
-    .from("h1", {opacity: 0, duration: 1, delay: 1.5})
+    .from("body", {autoAlpha: 0}, "<")
+    .from("h1", {opacity: 0, y: 10, duration: 0.7})
     .from(".table-of-contents li", {pointerEvents: "none", opacity: 0, x: -10, stagger: 0.1, duration: 1}, "<1")
-    .from(".main-content", {height: 0, duration: 1.5}, )
+    .from(".main-content", {height: 0, duration: 2})
     .from(".main-content-section", {opacity: 0, duration: 1}, "<")
     .from(".navbar", {opacity: 0, duration: 1}, "<0.5")
     .from(".footer", {opacity: 0, duration: 1}, "<")
     .set("body", {overflow: "scroll"}, "<")
 }
+
+// ----- Do animation when page elements load -----
+window.addEventListener("load", () => {
+  init();
+});
