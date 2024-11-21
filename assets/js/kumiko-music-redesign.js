591d6f725714b21db935d1f42a6db622499c8f0f
Guillermo Martin <gscalica@gmail.com>
2024-11-21 14:19:25 -0800

    Add page load aniamtion to 'Kumiko music redesign' page

--- assets/js/kumiko-music-redesign.js
+++ assets/js/kumiko-music-redesign.js
@@ -1,5 +1,26 @@
 console.log("connected to kumiko music redesign!");
 
+// ---------- Page load animation function ----------
+const init = () => {
+  gsap.timeline()
+    .from("body", {autoAlpha: 0})
+    .to("body", {backgroundColor: "#000", duration: 1})
+    .set("body", {overflow: "hidden"})
+    .from("h1", {opacity: 0, y: 10, duration: 0.7})
+    .from(".table-of-contents li", {pointerEvents: "none", opacity: 0, x: -10, stagger: 0.1, duration: 1}, "<1")
+    .from(".additional-info", {opacity: 0, y: 10, duration: 0.7}, "<1")
+    .from(".main-content-section", {opacity: 0, stagger: 0.1, duration: 1})
+    .from(".navbar", {opacity: 0, duration: 1}, "<0.5")
+    .from(".footer", {opacity: 0, duration: 1}, "<")
+    .set("body", {overflow: "scroll"}, "<")
+}
+
+// ----- Do animation when page elements load -----
+window.addEventListener("load", () => {
+  init();
+});
+
+
 // ---------- Fade in section animation (767px and below) ----------
 mm.add("(max-width: 767px", () => {
   // scrollTrigger fadeIn animation (for mobile)
