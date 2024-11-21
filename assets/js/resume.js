5a336c6a4010734b53470ba7334ee942d481307d
Guillermo Martin <gscalica@gmail.com>
2024-11-21 14:01:05 -0800

    Add page transition animation to 'Resume' page

---
+++ assets/js/resume.js
@@ -0,0 +1,19 @@
+console.log("connected to resume!!!");
+
+// ---------- Page load animation function ----------
+const init = () => {
+  gsap.timeline()
+    .set("body", {overflow: "hidden"})
+    .from("body", {autoAlpha: 0}, "<")
+    .from("h1", {opacity: 0, y: 10, duration: 0.7})
+    .from(".table-of-contents li", {pointerEvents: "none", opacity: 0, x: -10, stagger: 0.1, duration: 1}, "<1")
+    .from(".main-content-section", {opacity: 0, stagger: 0.1, duration: 1})
+    .from(".navbar", {opacity: 0, duration: 1}, "<0.5")
+    .from(".footer", {opacity: 0, duration: 1}, "<")
+    .set("body", {overflow: "scroll"}, "<")
+}
+
+// ----- Do animation when page elements load -----
+window.addEventListener("load", () => {
+  init();
+});
