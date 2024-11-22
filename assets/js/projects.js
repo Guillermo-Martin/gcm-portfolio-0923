5a61c41d89c875d46b1cf3aecef3d2a966f698ba
Guillermo Martin <gscalica@gmail.com>
2024-11-22 14:46:05 -0800

    Add navbar page transition animations to 'projects' page

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -19,3 +19,64 @@ const init = () => {
 window.addEventListener("load", () => {
   init();
 });
+
+
+
+
+// ---------------------------------------------
+// ---------- Navbar links page transition animations ----------
+// get navbar links
+const navbarContainer = document.querySelector(".navbar ul");
+
+/**
+  * This is the Greensock page transition animation timeline.
+  * @param {object} event - the "event" object when a user clicks on a link
+  * @param {string} elem1 - the first portfolio link you want to fade away
+  * @param {string} elem2 - the second portfolio link you want to fade away
+*/
+const transitionTimeline = (event, elem1, elem2) => {
+  const targetId = event.target.id;
+  const target = event.target.href;
+  const animationOptions = {opacity: 0, duration: 0.7};
+
+  gsap.timeline()
+    .to(".sidenav-content", animationOptions)
+    .to(".main-content", animationOptions, "<")
+    .to(".footer", animationOptions, "<")
+    .to(`#${elem1}`, animationOptions, "<")
+    .to(`#${elem2}`, animationOptions, "<")
+    .call(() => {
+      // get all links
+      let allLinks = document.querySelectorAll("a");
+
+      // loop through the links and add a class for no pointer events
+      for(let j = 0; j < allLinks.length; j++) {
+        allLinks[j].style.pointerEvents = "none";
+      }
+    })
+    .to(`#transition-${targetId}`, {opacity: 0, y: -5, duration: 0.5, delay: 0.8})
+    .call(() => {
+      window.location.href = target;
+    });
+}
+
+// ----- Page transition implementation -----
+navbarContainer.addEventListener("click", (event) => {
+  // prevent default link behavior
+  event.preventDefault();
+
+  // home link
+  if(event.target.id === "home") {
+    transitionTimeline(event, "about", "resume");
+  };
+
+  // about link
+  if(event.target.id === "about") {
+    transitionTimeline(event, "home", "resume");
+  };
+
+  // resume link
+  if(event.target.id === "resume") {
+    transitionTimeline(event, "home", "about");
+  };
+});
