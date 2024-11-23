cd25f2cd52fba18ce16d732e42c048dbf41eab97
Guillermo Martin <gscalica@gmail.com>
2024-11-22 16:45:11 -0800

    Refine 'kumiko music redesign' page load animation, revise function to change section title

--- assets/js/kumiko-music-redesign.js
+++ assets/js/kumiko-music-redesign.js
@@ -99,10 +99,8 @@ mm.add("(max-width: 992px)", () => {
     .to(".sidenav", {backgroundColor: "#000", duration: 1}, "<")
     .set("body", {overflow: "hidden"})
     .from("h1", {opacity: 0, y: 10, duration: 0.7})
-    .from(".table-of-contents li", {pointerEvents: "none", opacity: 0, x: -10, stagger: 0.1, duration: 1}, "<1")
-    .from(".additional-info", {opacity: 0, y: 10, duration: 0.7}, "<1")
+    .from(".current-section", {opacity: 0, y: 10, duration: 0.7})
     .from(".main-content-section", {opacity: 0, stagger: 0.1, duration: 1})
-    .from(".navbar", {opacity: 0, duration: 1}, "<0.5")
     .from(".mobile-nav-icon", {opacity: 0, pointerEvents: "none", duration: 0.7}, "<")
     .from(".footer", {opacity: 0, duration: 1}, "<")
     .set("body", {overflow: "scroll"}, "<")
@@ -121,17 +119,12 @@ mm.add("(max-width: 767px)", () => {
     const sectionId = section.id;
 
     // fade in animation function
-    const fadeInLeft = () => {
+    const changeSectionTitle = () => {
       // get current section
       const currentSectionTitle = document.querySelector(".current-section");
 
       // change it's textcontent
       currentSectionTitle.textContent = sectionHeader;
-
-      // fade in animation
-      gsap.timeline()
-        .set(".current-section", {opacity: 0, x: -10})
-        .to(".current-section", {opacity: 1, x: 0, duration: 0.8, delay: 2.7})
     };
 
     // create a scrollTrigger for each of the sections
@@ -140,8 +133,8 @@ mm.add("(max-width: 767px)", () => {
       markers: true,
       start: "top 26.4%",
       end: "bottom 75%",
-      onEnter: fadeInLeft,
-      onEnterBack: fadeInLeft
+      onEnter: changeSectionTitle,
+      onEnterBack: changeSectionTitle
     });
   });
 }); 
