e5652d071dca56ca008d1ae65e3fc58e058cf70d
Guillermo Martin <gscalica@gmail.com>
2025-01-07 09:50:47 -0800

    Create function for mobile page load animation for 'About' page

--- assets/js/about.js
+++ assets/js/about.js
@@ -95,18 +95,9 @@ navbarContainer.addEventListener("click", (event) => {
 });
 
 // ---------- page load animation at 992px and below ----------
-mm.add("(max-width: 992px)", () => {
-  window.addEventListener("pageshow", (event) => {
-    if (event.persisted) {
-      console.log("pageshow triggered: Page loaded from cache.");
-      alert("pageshow event fired! Page loaded from cache.");
-    } else {
-      console.log("pageshow triggered: Page loaded normally.");
-      alert("pageshow event fired! Page loaded normally.");
-    }
-
-    // timeline
-    gsap.timeline()
+// ----- Mobile page load animation function -----
+const mobileInit = () => {
+  gsap.timeline()
     .from("body", {autoAlpha: 0})
     .set("body", {overflow: "hidden"})
     .from("h1", {opacity: 0, y: 10, duration: 0.7})
@@ -117,6 +108,40 @@ mm.add("(max-width: 992px)", () => {
     .from(".footer", {opacity: 0, duration: 1}, "<")
     .from(".table-of-contents li", {pointerEvents: "none"}, "<")
     .set("body", {overflow: "scroll"}, "<")
+};
+
+mm.add("(max-width: 992px)", () => {
+  // See if page is being loaded from cache.  If so, reset all elements from the page transition animation
+  // then play the entrance animation.
+  window.addEventListener("pageshow", (event) => {
+    if (event.persisted) {
+      console.log("pageshow triggered: Page loaded from cache.");
+      // alert("pageshow event fired! Page loaded from cache.");
+
+      // reset all elements to initial state
+
+      // play the entrance animation
+      mobileInit();
+    } else {
+      console.log("pageshow triggered: Page loaded normally.");
+      // alert("pageshow event fired! Page loaded normally.");
+
+      // Otherwise, play the entrance animation
+      mobileInit();
+    }
+
+    // timeline
+    // gsap.timeline()
+    // .from("body", {autoAlpha: 0})
+    // .set("body", {overflow: "hidden"})
+    // .from("h1", {opacity: 0, y: 10, duration: 0.7})
+    // .from(".table-of-contents li", {opacity: 0, x: -10, stagger: 0.1, duration: 1}, "<1")
+    // .from(".current-section", {opacity: 0, y: 10, duration: 0.7})
+    // .from(".main-content-section", {opacity: 0, stagger: 0.1, duration: 1})
+    // .from(".mobile-nav-icon", {opacity: 0, pointerEvents: "none", duration: 0.7}, "<")
+    // .from(".footer", {opacity: 0, duration: 1}, "<")
+    // .from(".table-of-contents li", {pointerEvents: "none"}, "<")
+    // .set("body", {overflow: "scroll"}, "<")
   });
 });
 
