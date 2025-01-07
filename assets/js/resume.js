7e9062c0d80875853b1b65992ae617a56033eebe
Guillermo Martin <gscalica@gmail.com>
2025-01-07 14:23:08 -0800

    Add 'pageshow' event listener to 'resume' page at 992px, and add code for testing

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -97,18 +97,29 @@ navbarContainer.addEventListener("click", (event) => {
 
 // ---------- page load animation at 992px and below ----------
 mm.add("(max-width: 992px)", () => {
-  window.addEventListener("load", () => {
-    gsap.timeline()
-    .from("body", {autoAlpha: 0})
-    .set("body", {overflow: "hidden"})
-    .from("h1", {opacity: 0, y: 10, duration: 0.7})
-    .from(".table-of-contents li", {opacity: 0, x: -10, stagger: 0.1, duration: 1}, "<1")
-    .from(".current-section", {opacity: 0, y: 10, duration: 0.7})
-    .from(".main-content-section", {opacity: 0, stagger: 0.1, duration: 1})
-    .from(".mobile-nav-icon", {opacity: 0, pointerEvents: "none", duration: 0.7}, "<")
-    .from(".footer", {opacity: 0, duration: 1}, "<")
-    .from(".table-of-contents li", {pointerEvents: "none"}, "<")
-    .set("body", {overflow: "scroll"}, "<")
+  // See if page is being loaded from cache.  If so, reset all elements from the page transition animation
+  // then play the entrance animation.
+  // window.addEventListener("load", () => {
+  window.addEventListener("pageshow", (event) => {
+    if (event.persisted) {
+      console.log("pageshow triggered: Page loaded from cache.");
+      alert("pageshow event fired! Page loaded from cache.");
+    } else {
+      console.log("pageshow triggered: Page loaded normally.");
+      alert("pageshow event fired! Page loaded normally.");
+    }
+
+    // gsap.timeline()
+    // .from("body", {autoAlpha: 0})
+    // .set("body", {overflow: "hidden"})
+    // .from("h1", {opacity: 0, y: 10, duration: 0.7})
+    // .from(".table-of-contents li", {opacity: 0, x: -10, stagger: 0.1, duration: 1}, "<1")
+    // .from(".current-section", {opacity: 0, y: 10, duration: 0.7})
+    // .from(".main-content-section", {opacity: 0, stagger: 0.1, duration: 1})
+    // .from(".mobile-nav-icon", {opacity: 0, pointerEvents: "none", duration: 0.7}, "<")
+    // .from(".footer", {opacity: 0, duration: 1}, "<")
+    // .from(".table-of-contents li", {pointerEvents: "none"}, "<")
+    // .set("body", {overflow: "scroll"}, "<")
   });
 });
 
