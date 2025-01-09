a994b797d945660ab14e183e511a984f9380db7a
Guillermo Martin <gscalica@gmail.com>
2025-01-08 18:33:27 -0800

    Testing - add page entrance timeline to 'else' statement for desktop

--- assets/js/about.js
+++ assets/js/about.js
@@ -73,8 +73,20 @@ mm.add("(min-width: 993px)", () => {
     } else {
       console.log("desktop page loaded normally.")
       // Otherwise, play the page entrance animation.
-      init();
+      // init();
       // desktopTimeline.play();
+
+      gsap.timeline() // TEST
+        .set("body", {overflow: "hidden"})
+        .from("body", {autoAlpha: 0}, "<")
+        .from("h1", {opacity: 0, y: 10, duration: 0.7})
+        .from(".table-of-contents li", {pointerEvents: "none", opacity: 0, x: -10, stagger: 0.1, duration: 1}, "<1")
+        .from(".main-content", {opacity: 0, duration: 0.5})
+        .from(".main-content-section", {opacity: 0, duration: 0.5}, "<0.4")
+        .from(".navbar", {pointerEvents: "none", opacity: 0, duration: 1}, "<0.5")
+        .from(".mobile-nav-icon", {opacity: 0, pointerEvents: "none", duration: 0.7}, "<")
+        .from(".footer", {opacity: 0, duration: 1}, "<")
+        .set("body", {overflow: "scroll"}, "<")
     };
   });
 });
