e4d13648122095e2a495a4d046a394b2e5fb9f96
Guillermo Martin <gscalica@gmail.com>
2026-03-13 11:34:26 -0700

    Style mobile nav

--- assets/js/case-study.js
+++ assets/js/case-study.js
@@ -2,17 +2,24 @@ console.log("on case study page!")
 
 
 // get elements
+let body = document.body;
 let hamburgerIcon = document.querySelector(".hamburger-icon");
 let mobileNavOverlay = document.querySelector(".mobile-nav-overlay");
 let closeIcon = document.querySelector(".close-icon");
 
 hamburgerIcon.addEventListener("click", () => {
+  // prevent user from scrolling while the menu is up
+  body.style.overflow = "hidden";
+
   // remove the "hide" class and add the "show" class
   mobileNavOverlay.classList.remove("hide");
   mobileNavOverlay.classList.add("show");
 });
 
 closeIcon.addEventListener("click", () => {
+  // allow user to scroll when the menu is gone
+  body.style.overflow = "auto";
+
   // remove the "show" class and add the "hide" class
   mobileNavOverlay.classList.remove("show");
   mobileNavOverlay.classList.add("hide");
