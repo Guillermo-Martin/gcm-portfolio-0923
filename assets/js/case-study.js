0a46f97e1f15780b5270f762dc79ca325c7d5ce1
Guillermo Martin <gscalica@gmail.com>
2026-03-13 12:09:49 -0700

    Restructure desktop and mobile navbar, use position:fixed to keep navbar at the top

--- assets/js/case-study.js
+++ assets/js/case-study.js
@@ -4,12 +4,21 @@ console.log("on case study page!")
 // get elements
 let body = document.body;
 let hamburgerIcon = document.querySelector(".hamburger-icon");
-let mobileNavOverlay = document.querySelector(".mobile-nav-overlay");
 let closeIcon = document.querySelector(".close-icon");
+let mobileNavOverlay = document.querySelector(".mobile-nav-overlay");
 
 hamburgerIcon.addEventListener("click", () => {
   // prevent user from scrolling while the menu is up
   body.style.overflow = "hidden";
+
+  // closeIcon.classList.add("show");
+
+  // hide the hamburger icon, show the close icon
+  hamburgerIcon.classList.remove("show");
+  hamburgerIcon.classList.add("hide");
+  closeIcon.classList.remove("hide");
+  closeIcon.classList.add("show");
+ 
 
   // remove the "hide" class and add the "show" class
   mobileNavOverlay.classList.remove("hide");
@@ -19,6 +28,12 @@ hamburgerIcon.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
   // allow user to scroll when the menu is gone
   body.style.overflow = "auto";
+
+  // hide the close icon, show the hamburger icon
+  closeIcon.classList.remove("show");
+  closeIcon.classList.add("hide");
+  hamburgerIcon.classList.remove("hide");
+  hamburgerIcon.classList.add("show");
 
   // remove the "show" class and add the "hide" class
   mobileNavOverlay.classList.remove("show");
