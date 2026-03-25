607e411999a8d9bbd554594b25bc49b4ad220ba5
Guillermo Martin <gscalica@gmail.com>
2026-03-25 15:50:23 -0700

    Use 'transitionend' for 'Final designs' gallery main image transitions and fix gallery on mobile

--- assets/js/case-study.js
+++ assets/js/case-study.js
@@ -62,26 +62,33 @@ highFidelitySlideshow.startSlideshow();
 for(let thumbnail of finalDesignsDesktopThumbnails){
   // when you click on a thumbnail, change the src and alt of the main image
   thumbnail.addEventListener("click", () => {
-    // fadeout the previous image...
+    // 1. remove the "active" class on the finalDesignImage (will cause the image to fade out)
     finalDesignImage.classList.remove("active");
 
-    // ...then swap in image and fade in
-    setTimeout(() => {
-      // swap image
-      finalDesignImage.src = thumbnail.src;
-      finalDesignImage.alt = thumbnail.alt;
+    // transitionend:  https://jh3y.medium.com/using-transition-events-in-javascript-883f2e8588d9
+    // transitionend:  https://www.brucebrotherton.com/blog/transitions-and-event-listeners/#:~:text=TL;DR,Read%20on.
+    finalDesignImage.addEventListener("transitionend", (event) => {
+      // 2. check to see see if the opacity transition has finished (we only care about the "opacity" property)
+      if(event.propertyName === "opacity"){
 
-      // fade in
-      finalDesignImage.classList.add("active");
-    }, 200);
-    
+        // 3. if it has, change the image and alt tag
+        finalDesignImage.src = thumbnail.src;
+        finalDesignImage.alt = thumbnail.alt;
 
-    // remove any active states
+        // 4. then add the "active" class to fade in the image
+        finalDesignImage.classList.add("active");
+      } else {
+        // otherwise, do nothing
+        return;
+      };
+    }, { once: true });
+
+    // 5. remove any active states fom the thumbnails
     for(let thumbnail of finalDesignsDesktopThumbnails){
       thumbnail.classList.remove("active");
     };
 
-    // add an active state to the button you clicked on
+    // 6. add an "active" state to the thumbnail you clicked on
     thumbnail.classList.add("active");
   });
 };
@@ -90,16 +97,34 @@ for(let thumbnail of finalDesignsDesktopThumbnails){
 for(let thumbnail of finalDesignsMobileThumbnails){
   // when you click on a thumbnail, change the src and alt of the main image
   thumbnail.addEventListener("click", () => {
-    finalDesignImage.src = thumbnail.src;
-    finalDesignImage.alt = thumbnail.alt;
+    // 1. remove the "active" class on the finalDesignImage (will cause the image to fade out)
+    finalDesignImage.classList.remove("active");
 
-    // remove any active states
-  for(let thumbnail of finalDesignsMobileThumbnails){
-    thumbnail.classList.remove("active");
-  };
+    // transitionend:  https://jh3y.medium.com/using-transition-events-in-javascript-883f2e8588d9
+    // transitionend:  https://www.brucebrotherton.com/blog/transitions-and-event-listeners/#:~:text=TL;DR,Read%20on.
+    finalDesignImage.addEventListener("transitionend", (event) => {
+      // 2. check to see see if the opacity transition has finished (we only care about the "opacity" property)
+      if(event.propertyName === "opacity"){
 
-  // add an active state to the button you clicked on
-  thumbnail.classList.add("active");
+        // 3. if it has, change the image and alt tag
+        finalDesignImage.src = thumbnail.src;
+        finalDesignImage.alt = thumbnail.alt;
+
+        // 4. then add the "active" class to fade in the image
+        finalDesignImage.classList.add("active");
+      } else {
+        // otherwise, do nothing
+        return;
+      };
+    }, { once: true });
+
+    // 5. remove any active states fom the thumbnails
+    for(let thumbnail of finalDesignsMobileThumbnails){
+      thumbnail.classList.remove("active");
+    };
+
+    // 6. add an "active" state to the thumbnail you clicked on
+    thumbnail.classList.add("active");
   });
 };
 
