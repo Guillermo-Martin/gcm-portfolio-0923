2a2b81572ae3312436fbe37e92bc6a01ea4a7a86
Guillermo Martin <gscalica@gmail.com>
2026-04-09 16:20:21 -0700

    Create slideshows for 'navigation', 'design and layout', and 'readability' sections

--- assets/js/kumiko-music-redesign.js
+++ assets/js/kumiko-music-redesign.js
@@ -1,5 +1,24 @@
 console.log("on kumi page")
 
+import { Slideshow } from "./Slideshow.js";
+
+// get the images for the "navigation" slideshow
+const navigationSlides = document.querySelectorAll(".navigation .slideshow img");
+const designSlides = document.querySelectorAll(".design-and-layout .slideshow img");
+const readabilitySlides = document.querySelectorAll(".readability .slideshow img");
+
+// create a new slideshow
+const navSlideshow = new Slideshow(navigationSlides);
+const designSlideshow = new Slideshow(designSlides);
+const readabilitySlideshow = new Slideshow(readabilitySlides);
+
+// run the slideshow
+navSlideshow.startSlideshow();
+designSlideshow.startSlideshow();
+readabilitySlideshow.startSlideshow();
+
+
+
 
 
 // // ---------- Page load animation function ----------
