8b2e5133f020e23d2c45a64518c493c940f12f2b
Guillermo Martin <gscalica@gmail.com>
2026-04-30 17:02:24 -0700

    Add content to 'More refinement' section and style

--- assets/js/bakery-board.js
+++ assets/js/bakery-board.js
@@ -7,15 +7,24 @@ const readabilitySlides = document.querySelectorAll(".readability .slideshow img
 const clearLanguageSlides = document.querySelectorAll(".clearer-language .slideshow img");
 const homepageSlides = document.querySelectorAll(".homepage .slideshow img");
 const responsiveSlides = document.querySelectorAll(".responsive-design .slideshow img");
+const dashboardSlides = document.querySelectorAll(".user-dashboard .slideshow img");
+const easierNavSlides = document.querySelectorAll(".easier-nav .slideshow img");
+const quickAddSlides = document.querySelectorAll(".quick-add .slideshow img");
 
 // create a new slideshow
 const readabilitySlideshow = new Slideshow(readabilitySlides);
 const clearLanguageSlideshow = new Slideshow(clearLanguageSlides);
 const homepageSlideshow = new Slideshow(homepageSlides);
 const responsiveSlideshow = new Slideshow(responsiveSlides);
+const dashboardSlideshow = new Slideshow(dashboardSlides);
+const easierNavSlideshow = new Slideshow(easierNavSlides);
+const quickAddSlideshow = new Slideshow(quickAddSlides);
 
 // run the slideshow
 readabilitySlideshow.startSlideshow();
 clearLanguageSlideshow.startSlideshow();
 homepageSlideshow.startSlideshow();
 responsiveSlideshow.startSlideshow();
+dashboardSlideshow.startSlideshow();
+easierNavSlideshow.startSlideshow();
+quickAddSlideshow.startSlideshow();
